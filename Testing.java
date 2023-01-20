d8754e9ee23456912b1cc0a6a26c6c55ac3fe1a0
Aman Budhrani <76804491+amanbudhrani17@users.noreply.github.com>
2023-01-20 23:10:09 +0530

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 public class Testing {
     @Test
     public void testPart3(){
+//         Date Format DDMMMYYYY
         assertEquals("CoconutValley", Solution.sol("Regular",new String[]{"16Mar2009", "17Mar2009", "18Mar2009"}));
         assertEquals("AakulamLake", Solution.sol("Regular",new String[]{"20Mar2009", "21Mar2009", "22Mar2009"}));
         assertEquals("VeliBeach", Solution.sol("Rewards",new String[]{"26Mar2009", "27Mar2009", "28Mar2009"}));
